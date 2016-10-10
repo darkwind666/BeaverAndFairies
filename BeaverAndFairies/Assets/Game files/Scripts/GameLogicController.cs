2fd87324c5537a2ff7af084ecb90c749a6ccc396
darkwinddev <darkwinddev@gmail.comgit config --global user.email arteomchik@gmail.com>
2016-10-11 01:19:49 +0300

    add new block types

---
+++ BeaverAndFairies/Assets/Game files/Scripts/GameLogicController.cs
@@ -0,0 +1,202 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLogicController : MonoBehaviour {
+
+	public float blocksSpeed;
+	public int boardHeight;
+	public GameObject blockExample;
+	public Text scoreLabel;
+	float _blockHeight;
+	float _loseHeight;
+	bool _lose;
+	int _score;
+
+	public GameObject[] blockTemplates;
+	Queue<GameObject> _currentBlocks;
+
+	public int spawnTime;
+	int _currentSpawnTime;
+
+	SwipeDirectionController _swipeDirectionController;
+
+	void Start () {
+	
+		Renderer renderer = blockExample.GetComponent<Renderer>();
+		_blockHeight = renderer.bounds.size.y;
+		_loseHeight = (_blockHeight * boardHeight);
+		_currentSpawnTime = 0;
+		_currentBlocks = new Queue<GameObject>();
+		_score = 0;
+		_swipeDirectionController = new SwipeDirectionController();
+	}
+		
+	void Update () {
+
+		if(_lose == false)
+		{
+			spawnNewBlock();
+			_swipeDirectionController.getTouchDirection();
+			getUserInput();
+			moveDownBlocks();
+			checkGameResult();
+		}
+
+		scoreLabel.text = _score.ToString();
+	}
+
+	void spawnNewBlock()
+	{
+		_currentSpawnTime++;
+
+		if(_currentSpawnTime >= spawnTime)
+		{
+			_currentSpawnTime = 0;
+			int blockIndex = Random.Range(0, blockTemplates.Length);
+			GameObject block = Instantiate(blockTemplates[blockIndex], transform.position, Quaternion.identity) as GameObject;
+			block.transform.SetParent(transform.parent, false);
+			block.transform.position = transform.position;
+			_currentBlocks.Enqueue(block);
+		}
+	}
+
+	void getUserInput()
+	{
+
+		if(_currentBlocks.Count > 0) 
+		{
+			GameObject firstBlock = _currentBlocks.Peek();
+			BlockTypeController blockTypeComponent = firstBlock.GetComponent<BlockTypeController>();
+			int blockType = blockTypeComponent.blockType;
+
+			if(blockType == 1 && _swipeDirectionController.mMessageIndex == 3)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 2 && _swipeDirectionController.mMessageIndex == 4)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 3 && _swipeDirectionController.mMessageIndex == 2)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 4 && _swipeDirectionController.mMessageIndex == 1)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 5 && Input.GetMouseButtonDown(0) == true)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 6 && Input.touchCount == 2)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 7 && Input.touchCount == 3)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if(blockType == 8 && Input.touchCount == 4)
+			{
+				removeFirstBlock();
+				_score += 1;
+			}
+
+			if (Input.GetMouseButtonDown (0) == true && blockType > 5) {
+				blockTypeComponent.blockType -= 1;
+				GameObject firstChild = firstBlock.transform.GetChild(0).gameObject;
+				Destroy(firstChild);
+			}
+		}
+
+		_swipeDirectionController.mMessageIndex = 0;
+	}
+
+	void removeFirstBlock()
+	{
+		GameObject firstBlock = _currentBlocks.Dequeue();
+		Destroy(firstBlock);
+
+		foreach (GameObject block in _currentBlocks) 
+		{
+			BlockTypeController blockTypeComponent = firstBlock.GetComponent<BlockTypeController>();
+			if (blockTypeComponent.placed == true) 
+			{
+				block.transform.localPosition = new Vector3(block.transform.localPosition.x, block.transform.localPosition.y - _blockHeight, 0);
+			}
+		}
+	}
+
+	void moveDownBlocks()
+	{
+		foreach (GameObject block in _currentBlocks) 
+		{
+			BlockTypeController blockTypeComponent = block.GetComponent<BlockTypeController>();
+
+			if (blockTypeComponent.placed == false) 
+			{
+				tryToMoveDownBlock(block);
+			}
+		}
+	}
+
+	void tryToMoveDownBlock(GameObject aBlock)
+	{
+		aBlock.transform.localPosition = new Vector3(aBlock.transform.localPosition.x, aBlock.transform.localPosition.y - blocksSpeed, 0);
+
+		BlockTypeController blockTypeComponent = aBlock.GetComponent<BlockTypeController>();
+		Renderer renderer = aBlock.GetComponent<Renderer>();
+
+		float loseHeight = (_blockHeight / 2);
+
+		if (aBlock.transform.localPosition.y < loseHeight) {
+			blockTypeComponent.placed = true;
+			aBlock.transform.localPosition = new Vector3 (aBlock.transform.localPosition.x, aBlock.transform.localPosition.y + blocksSpeed, 0);
+		} else {
+			foreach (GameObject blockForCollision in _currentBlocks)
+			{
+				if (aBlock != blockForCollision) 
+				{
+					Renderer blockForCollisionRenderer = blockForCollision.GetComponent<Renderer>();
+					if(renderer.bounds.Intersects(blockForCollisionRenderer.bounds) == true || aBlock.transform.localPosition.y < loseHeight)
+					{
+						blockTypeComponent.placed = true;
+						aBlock.transform.localPosition = new Vector3(blockForCollision.transform.localPosition.x, blockForCollision.transform.localPosition.y + _blockHeight, 0);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	void checkGameResult()
+	{
+		foreach (GameObject block in _currentBlocks) 
+		{
+			BlockTypeController blockTypeComponent = block.GetComponent<BlockTypeController>();
+
+			if (blockTypeComponent.placed == true && block.transform.localPosition.y > (_loseHeight * _blockHeight)) 
+			{
+				_lose = true;
+				break;
+			}
+		}
+	}
+}
---
+++ BeaverAndFairies/Assets/Game files/Scripts/SwipeDirectionController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeDirectionController {
+
+	Vector3 _lastAcceleration;
+	Vector3 _accelerationDelta;
+
+	private const int mMessageWidth  = 200;
+	private const int mMessageHeight = 64;
+
+	private readonly Vector2 mXAxis = new Vector2(1, 0);
+	private readonly Vector2 mYAxis = new Vector2(0, 1);
+
+	private readonly string [] mMessage = {
+		"",
+		"Swipe Left",
+		"Swipe Right",
+		"Swipe Top",
+		"Swipe Bottom"
+	};
+
+	public int mMessageIndex = 0;
+
+	// The angle range for detecting swipe
+	private const float mAngleRange = 30;
+
+	// To recognize as swipe user should at lease swipe for this many pixels
+	private const float mMinSwipeDist = 5.0f;
+
+	// To recognize as a swipe the velocity of the swipe
+	// should be at least mMinVelocity
+	// Reduce or increase to control the swipe speed
+	private const float mMinVelocity  = 2000.0f;
+
+	private Vector2 mStartPosition;
+	private float mSwipeStartTime;
+
+	public SwipeDirectionController() {
+		_lastAcceleration = new Vector3(0, 0, 0);
+	}
+
+	public void getTouchDirection()
+	{
+
+		// Mouse button down, possible chance for a swipe
+		if (Input.GetMouseButtonDown(0)) {
+			// Record start time and position
+			mStartPosition = new Vector2(Input.mousePosition.x,
+				Input.mousePosition.y);
+			mSwipeStartTime = Time.time;
+		}
+
+		// Mouse button up, possible chance for a swipe
+		if (Input.GetMouseButtonUp(0)) {
+			float deltaTime = Time.time - mSwipeStartTime;
+
+			Vector2 endPosition  = new Vector2(Input.mousePosition.x,
+				Input.mousePosition.y);
+			Vector2 swipeVector = endPosition - mStartPosition;
+
+			float velocity = swipeVector.magnitude/deltaTime;
+
+			if (velocity > mMinVelocity &&
+				swipeVector.magnitude > mMinSwipeDist) {
+				// if the swipe has enough velocity and enough distance
+
+				swipeVector.Normalize();
+
+				float angleOfSwipe = Vector2.Dot(swipeVector, mXAxis);
+				angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+
+				// Detect left and right swipe
+				if (angleOfSwipe < mAngleRange) {
+					OnSwipeRight();
+				} else if ((180.0f - angleOfSwipe) < mAngleRange) {
+					OnSwipeLeft();
+				} else {
+					// Detect top and bottom swipe
+					angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
+					angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+					if (angleOfSwipe < mAngleRange) {
+						OnSwipeTop();
+					} else if ((180.0f - angleOfSwipe) < mAngleRange) {
+						OnSwipeBottom();
+					} else {
+						mMessageIndex = 0;
+					}
+				}
+			}
+		}
+
+	}
+
+	private void OnSwipeLeft() {
+		mMessageIndex = 1;
+	}
+
+	private void OnSwipeRight() {
+		mMessageIndex = 2;
+	}
+
+	private void OnSwipeTop() {
+		mMessageIndex = 3;
+	}
+
+	private void OnSwipeBottom() {
+		mMessageIndex = 4;
+	}
+}
