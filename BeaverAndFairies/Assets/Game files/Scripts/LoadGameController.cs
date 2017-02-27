5acb5c026b63c811d2423b1ec3c03c3e943f9bbd
darkwinddev <darkwinddev@gmail.comgit config --global user.email arteomchik@gmail.com>
2017-02-28 01:28:40 +0300

    fix Facebook warning

--- BeaverAndFairies/Assets/Game files/Scripts/LoadGameController.cs
+++ BeaverAndFairies/Assets/Game files/Scripts/LoadGameController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Facebook.Unity;
+using Fabric.Crashlytics;
 
 //using UnityEditor;
 //
@@ -26,6 +28,31 @@ public class LoadGameController : MonoBehaviour {
     Image circularLoader;
 	bool _startGameLoading;
 	int _currentTimeInterval;
+
+	void Awake ()
+	{
+		if (!FB.IsInitialized) {
+			FB.Init(InitCallback, OnHideUnity);
+		} else {
+			FB.ActivateApp();
+		}
+	}
+
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) {
+			FB.ActivateApp();
+		}
+	}
+
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}
+	}
 
     void Start ()
     {
